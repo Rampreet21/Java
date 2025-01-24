7c797cdf6c0bdadba78077dd74b1373fc590edc5
Rampreet21 <rr6975871@gmail.com>
2025-01-24 16:55:23 +0530

    me

---
+++ i_helo.java
@@ -0,0 +1,5 @@
+public class i_helo {
+    public static void main(String[] args) {
+     System.out.print("Hello");
+    }    
+}
---
+++ ii_variables.java
@@ -0,0 +1,8 @@
+public class ii_variables {
+    public static void main(String[] args) {
+        int a = 20;
+        
+        System.out.print("variables a: "+a+" int");
+
+    }
+}
