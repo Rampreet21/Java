adfd10bc1c99e3f18bcf73e6fa9d77969ef31b5a
Rampreet21 <rr6975871@gmail.com>
2025-01-26 17:14:37 +0530

    me

---
+++ v_calculate.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class v_calculate {
+    public static void main(String[] args){
+        Scanner dlip = new Scanner(System.in);
+
+        System.out.println("      Enter Your Marks");
+        System.out.println("Math: ");
+        int M = dlip.nextInt();
+
+        System.out.println("English: ");
+        int E = dlip.nextInt();
+        
+        System.out.println("Punjabi: ");
+        int P = dlip.nextInt();
+        
+        System.out.println("Hindi: ");
+        int H = dlip.nextInt();
+
+        
+
+    }
+}
